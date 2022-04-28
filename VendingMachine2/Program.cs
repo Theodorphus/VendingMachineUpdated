25795a4952b42166e9e1b5e06e741013f746a15a
Theodorphus <42559185+Theodorphus@users.noreply.github.com>
2022-04-28 16:38:50 +0200

    Add project files.

---
+++ VendingMachine2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using VendingMachine2;
+
+namespace VendingMachine2;
+
+
+public class Program
+{
+
+    static void Main(string[] args)
+    {
+
+
+        var vendingMachine = new VendingMachine();
+
+
+
+        Console.WriteLine("Please insert one of the amounts: (1, 5, 10, 20, 50, 100, 500, 1000)");
+        vendingMachine.InsertMoney(Convert.ToInt32(Console.ReadLine()));
+
+        while (vendingMachine.checkTotal() == true)
+        {
+
+            vendingMachine.ShowAll();
+
+            Console.ReadLine();
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+}
